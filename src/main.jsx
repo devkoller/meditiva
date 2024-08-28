aa6548624f1bdeee041ee1c603f47dd6362482af
Julio Mariscal <devkoller.jm@gmail.com>
2024-08-28 17:23:35 -0600

    add shapes and animations

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import AOS from 'aos'
 
 import './style/output.css'
 import 'react-multi-carousel/lib/styles.css'
 import './style/custom.css'
+import 'aos/dist/aos.css'
+
+AOS.init({
+  duration: 1000,
+  easing: 'ease-in-out',
+  once: true,
+  mirror: false
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
